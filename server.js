f83fb7d7a88243c19e808bd4c70b985484b30d9f
fzhygithub <6562157@qq.com>
2019-12-27 09:09:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello nodejs  20190731 18:08\n');
+  res.send('Hello nodejs  20191227\n');
   });
   app.listen(PORT, HOST);
   console.log(`Running on http://${HOST}:${PORT}`);
